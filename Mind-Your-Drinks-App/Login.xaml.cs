0adf1993dff9ea51e55de1947e0cb2b4d2252955
BETAKOTLETA <viktor.oleniuk@gmail.com>
2025-06-14 14:41:58 +0300

    Sign up button in app

--- Mind-Your-Drinks-App/Login.xaml.cs
+++ Mind-Your-Drinks-App/Login.xaml.cs
@@ -57,5 +57,44 @@ namespace Mind_Your_Drinks_App.Views
                 await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
             }
         }
+
+        private async void SignUpClicked(object sender, EventArgs e)
+        {
+
+            try
+            {
+                var url = "https://mind-your-drink-server20250611231310-h8aqf9a8cccgczfm.canadacentral-01.azurewebsites.net/User/CreateAccount";
+
+                var loginData = new
+                {
+                    Name = UsernameEntry.Text,
+                    Password = PasswordEntry.Text
+                };
+
+                var json = JsonSerializer.Serialize(loginData);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    var response = await client.PostAsync(url, content);
+                    var result = await response.Content.ReadAsStringAsync();
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        await Application.Current.MainPage.DisplayAlert("Create Successful", result, "OK");
+                    }
+                    else
+                    {
+                        await Application.Current.MainPage.DisplayAlert("Create Failed", result, "OK");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
     }
 }
