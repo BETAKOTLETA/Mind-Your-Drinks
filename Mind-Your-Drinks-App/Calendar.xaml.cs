f14004efc38a3270813afa3d5b9012334922e7a3
AndriiKudriavtsev <qwerdf0921@gmail.com>
2025-06-15 19:30:14 +0200

    Didn't notice that was coding in wrong branch. Calendar is not finished yet. No logic for calories and units.

--- Mind-Your-Drinks-App/Calendar.xaml.cs
+++ Mind-Your-Drinks-App/Calendar.xaml.cs
@@ -1,9 +1,10 @@
 namespace Mind_Your_Drinks_App;
-
 public partial class Calendar : ContentPage
-{
-	public Calendar()
-	{
-		InitializeComponent();
-	}
-}
+    {
+        public Calendar()
+        {
+            InitializeComponent();
+            calendar.DisplayDate = DateTime.Today; // set initial month
+        }
+    }
+
